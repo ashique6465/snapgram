462dabe9c0223e0c80c4b3e94eacb749b31c2c5c
ashique6465 <mdashiqueali71@gmail.com>
2024-09-07 03:46:36 +0530

    signup form

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import Authlayout from "./_auth/Authlayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* {public routes} */}
+        <Route element={<Authlayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SigninForm = () => {
+  return (
+    <div>
+      SigninForm
+      
+    </div>
+  )
+}
+
+export default SigninForm
